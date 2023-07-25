4fb16f3d33aa3cf5ec2b916b7f3dc2cdebc970c5
André Vandal <me@andrevandal.dev>
2023-07-25 00:48:56 -0300

    fix: build

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,18 @@ import compress from "astro-compress";
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [sitemap(), tailwind(), critters(), compress()],
+  integrations: [
+      sitemap(), 
+      tailwind(), 
+      critters(),
+      compress()
+    ],
+    // https://github.com/natemoo-re/astro-icon/issues/35
+    vite: {
+      resolve: {
+        alias: {
+          "svgo": import.meta.env.PROD ? "svgo/dist/svgo.browser.js" : "svgo"
+        }
+      }
+    }
 });
